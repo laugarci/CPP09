e272cb0028693ded64ce8b9dfdf9f9c7f69ce0ab
Laura Garcia Arch <garch.laura@gmail.com>
2024-03-04 11:35:36 +0100

    cpp09

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,20 +1,125 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 10:48:48 by laugarci          #+#    #+#             */
+/*   Updated: 2024/03/04 11:35:24 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "RPN.hpp"
 
-//DEBUG
-template<typename T> void printStack(std::stack<T> s) {
-    while (!s.empty()) {
-        std::cout << s.top() << " ";
-        s.pop();
-    }
-    std::cout << std::endl;
-}
-
-void RPN::trimSpaces(std::string &str)
+void RPN::trimSpaces(std::string& str)
 {
 	str.erase(0, str.find_first_not_of(" "));
 	str.erase(str.find_last_not_of(" ") + 1);
+}
+
+bool RPN::isNumber(std::string& str)
+{
+	if (str.empty())
+		return false;
+	size_t i = 0;
+	if ((str[i] == '-' || str[i] == '+') && std::isdigit(str[i + 1]))
+	{
+		++i;
+		if (i == str.size())
+			throw std::logic_error("not a valid number");
+	}
+	for (; i < str.size(); ++i)
+	{
+		if (!std::isdigit(str[i]))
+		{
+			if (str[i] != '+' && str[i] != '-' && str[i] != '/' && str[i] != '*')
+				throw std::logic_error("not a valid number");
+			else
+				return false;
+		}
+	}
+	return true;
+}
+
+void	RPN::doAddition(std::stack<int>& stack)
+{	
+	int num1;
+	int num2;
+
+	if (stack.size() >= 2)
+	{
+		num1 = stack.top();
+		stack.pop();
+		num2 = stack.top();
+		stack.pop();
+		stack.push(num2 + num1);
+	}
+	else
+		throw std::logic_error("logic error");
+}
+
+void	RPN::doSubtraction(std::stack<int>& stack)
+{
+	int num1;
+	int num2;
+
+	if (stack.size() >= 2)
+	{
+		num1 = stack.top();
+		stack.pop();
+		num2 = stack.top();
+		stack.pop();
+		stack.push(num2 - num1);
+	}
+	else
+		throw std::logic_error("logic error");
+}
+
+void	RPN::doMultiplication(std::stack<int>& stack)
+{
+	int num1;
+	int num2;
+
+	if (stack.size() >= 2)
+	{
+		num1 = stack.top();
+		stack.pop();
+		num2 = stack.top();
+		stack.pop();
+		stack.push(num2 * num1);
+	}
+	else
+		throw std::logic_error("logic error");
+}
+
+void	RPN::doDivision(std::stack<int>& stack)
+{
+	int num1;
+	int num2;
+
+	if (stack.size() >= 2)
+	{
+		num1 = stack.top();
+		stack.pop();
+		num2 = stack.top();
+		stack.pop();
+		stack.push(num2 / num1);
+	}
+	else
+		throw std::logic_error("logic error");
+}
+
+void	RPN::doOperations(std::stack<int>& stack, std::string& opt)
+{
+	if (!opt.compare("+"))
+		doAddition(stack);
+	else if (!opt.compare("-"))
+		doSubtraction(stack);
+	else if (!opt.compare("*"))
+		doMultiplication(stack);
+	else if (!opt.compare("\\"))
+		doDivision(stack);
 }
 
 void RPN::startRPN(std::string numbers)
@@ -22,30 +127,38 @@ void RPN::startRPN(std::string numbers)
 	size_t pos = 0;
 	size_t pos2;
 	std::stack<int> stack;
+	std::string next;
 
+	trimSpaces(numbers);
 	pos2 = numbers.find(" ");
 	if (pos2 == std::string::npos)
 		throw std::logic_error("at least two numbers and one operator needed");
 	while (pos2 != std::string::npos)
 	{
-		std::string next = numbers.substr(pos, pos2 - pos);
+		next = numbers.substr(pos, pos2 - pos);
 		trimSpaces(next);
 		if (!next.empty())
 		{
-			bool isNumber = true;
-			for (size_t i = 0; i < next.length(); ++i) {
-				if (!isdigit(next[i])) {
-					isNumber = false;
-					break;
-				}
-			}
-		if (isNumber) {
-			stack.push(atoi(next.c_str()));
-			}
+			if (isNumber(next))
+				stack.push(std::stoi(next));
+			else
+				doOperations(stack, next);
 		}
 		pos = pos2 + 1;
 		pos2 = numbers.find(" ", pos);
+		if (pos2 == std::string::npos)
+		{
+			next = numbers.substr(pos);
+			if (!next.empty())
+			{
+				if (isNumber(next))
+					stack.push(std::stoi(next));
+				else
+					doOperations(stack, next);
+			}
+		}
 	}
+	if (stack.size() > 1)
+		throw std::logic_error("logic error");
 	printStack(stack);
 }
-
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 10:48:46 by laugarci          #+#    #+#             */
+/*   Updated: 2024/03/04 11:35:28 by laugarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef RPN_HPP
 #define RPN_HPP
@@ -16,8 +27,24 @@ class RPN
 		~RPN(void);
 		RPN&	operator=(const RPN& rpn);
 		static void	trimSpaces(std::string& str);
+		static bool isNumber(std::string& str);
+		static void doOperations(std::stack<int>& stack, std::string& opt);
+		static void doAddition(std::stack<int>& stack);
+		static void doSubtraction(std::stack<int>& stack);
+		static void doMultiplication(std::stack<int>& stack);
+		static void doDivision(std::stack<int>& stack);
 	public:
 		static void startRPN(std::string str);
 };
+
+template<typename T>void printStack(std::stack<T> s)
+{
+	while (!s.empty())
+	{
+		std::cout << s.top() << " ";
+		s.pop();
+	}
+	std::cout << std::endl;
+}
 
 #endif
